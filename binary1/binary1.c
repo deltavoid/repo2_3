80030702d854c63d2c7653499a248ce6740ce403
root <zhangqianyu.sys@bytedance.com>
2022-01-04 01:17:21 +0800

    add binary1

--- binary1/binary1.c
+++ binary1/binary1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <repo2_2/library1/public/library1.h>
+#include <repo2_3/library1/public/library1.h>
 
 
 int main()
 {
-    repo2_2_library1_display();
+    repo2_3_library1_display();
 
     return 0;
 }
