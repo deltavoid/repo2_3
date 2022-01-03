fb206a0f697b6339fc13a4b1c3b8d405a2bdc0d8
root <zhangqianyu.sys@bytedance.com>
2022-01-04 01:24:03 +0800

    call repo2_2_library1_display

--- binary1/binary1.c
+++ binary1/binary1.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 #include <repo2_1/library1/public/library1.h>
+#include <repo2_2/library1/public/library1.h>
 #include <repo2_3/library1/public/library1.h>
 
 
 int main()
 {
     repo2_1_library1_display();
+
+    repo2_2_library1_display();
 
     repo2_3_library1_display();
 
